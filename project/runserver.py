7f674c4e45e7c80e71f79ad38656faff6049f6ea
isaog <76036735+watashiwaisadesu@users.noreply.github.com>
2024-10-08 20:40:07 +0500

    check..

--- project/runserver.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from app.config.settings import settings_env
-settings = settings_env
-
-if __name__ == "__main__":
-    host = settings.SERVER_HOST
-    port = settings.SERVER_PORT
-    uvicorn.run("app.main:app", host=host,port=int(port), reload=True)
