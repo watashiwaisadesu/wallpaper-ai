475c1747e7e3a1249d1c5a8ce4a107c3b62429f3
isaog <76036735+watashiwaisadesu@users.noreply.github.com>
2024-10-23 18:35:45 +0500

    asd

--- project/app/products/routes/leroymerlin.py
+++ project/app/products/routes/leroymerlin.py
@@ -6,6 +6,7 @@ from app.auth.services import get_current_user
 from app.products.services import fetch_wallpapers,fetch_tiles
 from app.core import get_async_db
 from app.products.responses import LeroyMerlinResponse
+from app.products.schemas import ProductRequestSchema
 
 
 product_router = APIRouter(
@@ -13,11 +14,11 @@ product_router = APIRouter(
     tags=["Products"]
 )
 
-@product_router.get("/wallpapers/", response_model=List[LeroyMerlinResponse])
-async def get_wallpapers(page: int = 1, db: AsyncSession = Depends(get_async_db), user = Depends(get_current_user)):
-    return await fetch_wallpapers(page, db)
+@product_router.post("/wallpapers/", response_model=List[LeroyMerlinResponse])
+async def get_wallpapers(request: ProductRequestSchema, db: AsyncSession = Depends(get_async_db), user = Depends(get_current_user)):
+    return await fetch_wallpapers(request.page, db)
 
 
-@product_router.get("/tiles/", response_model=List[LeroyMerlinResponse])
-async def get_tiles(page: int = 1, db: AsyncSession = Depends(get_async_db), user = Depends(get_current_user)):
-    return await fetch_tiles(page, db)
+@product_router.post("/tiles/", response_model=List[LeroyMerlinResponse])
+async def get_tiles(request: ProductRequestSchema, db: AsyncSession = Depends(get_async_db), user = Depends(get_current_user)):
+    return await fetch_tiles(request.page, db)
